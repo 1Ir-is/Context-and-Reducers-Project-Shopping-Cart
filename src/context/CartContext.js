2391411c6242a8971089ed3c593f830c85d6e9e3
1Ir-is <mhuynk1005@gmail.com>
2024-08-13 16:36:43 +0700

    feat: add localStorage

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,18 +1,22 @@
-import { createContext, useContext, useReducer } from "react"
+import { createContext, useContext, useReducer, useEffect } from "react";
 import { cartReducer } from "../reducer/cartReducer";
 
 const initialState = {
-    cartList: [],
+    cartList: JSON.parse(localStorage.getItem("cartList")) || [],
     total: 0
 }
 
 const CartContext = createContext(initialState);
 
 export const CartProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(cartReducer, initialState)
+    const [state, dispatch] = useReducer(cartReducer, initialState);
+
+    useEffect(() => {
+        localStorage.setItem("cartList", JSON.stringify(state.cartList));
+    }, [state.cartList]);
 
     const addToCart = (product) => {
-        const updatedCartList = state.cartList.concat(product);
+        const updatedCartList = [...state.cartList, product];
         updateTotal(updatedCartList);
 
         dispatch({
@@ -20,32 +24,29 @@ export const CartProvider = ({ children }) => {
             payload: {
                 products: updatedCartList
             }
-        })
+        });
     }
 
     const removeFromCart = (product) => {
         const updatedCartList = state.cartList.filter((currentProduct) => currentProduct.id !== product.id);
         updateTotal(updatedCartList);
-        
+
         dispatch({
             type: "REMOVE_FROM_CART",
             payload: {
                 products: updatedCartList
             }
-        })
+        });
     }
 
     const updateTotal = (products) => {
-        let total = 0;
-        products.forEach((product) => {
-            total += product.price;
-        })
+        let total = products.reduce((acc, product) => acc + product.price, 0);
         dispatch({
             type: "UPDATE_TOTAL",
             payload: {
                 total
             }
-        })
+        });
     }
 
     const value = {
@@ -66,4 +67,3 @@ export const useCart = () => {
     const context = useContext(CartContext);
     return context;
 }
-
